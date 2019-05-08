3f8ba490f978e2745f8a3bcc99e2e9e69ddce3bf
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2019-05-08 03:53:24 +0200

    compile works

--- src/main/java/sk/bsmk/hi/App.java
+++ src/main/java/sk/bsmk/hi/App.java
@@ -2,8 +2,10 @@ package sk.bsmk.hi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"sk.bsmk", "org.openapitools.configuration"})
 public class App {
 
   public static void main(String ... args) {
